f3002a6282c514981341fe65aad7264b84de3d5a
MattiaMad02 <mattia.madera@edu.unifi.it>
2025-07-07 16:16:56 +0200

    Preparazione su railway

--- BackEndProject/settings.py
+++ BackEndProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-3owpgu-x@(*yh(ts5hu!4zx8j7wyan0exka4wig*dg@k+sel&b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.app']
 
 
 # Application definition
@@ -141,3 +142,4 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 5,
 }
 CORS_ALLOW_ALL_ORIGINS = True
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
