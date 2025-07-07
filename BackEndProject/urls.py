46d0a0edf8cda4f3d7954b1a9ed4bb9fdca236eb
MattiaMad02 <mattia.madera@edu.unifi.it>
2025-07-07 16:54:05 +0200

    Preparazione su railway

--- BackEndProject/urls.py
+++ BackEndProject/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("Benvenuto nel backend!")
 urlpatterns = [
+path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('polls.urls')),
     path('api-token-auth/', obtain_auth_token),
